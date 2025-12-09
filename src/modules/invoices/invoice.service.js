7d7a8961e0f420d3ec35936025f6329715ab1cae
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-09 08:19:01 +0600

    invoices

--- src/modules/invoices/invoice.service.js
+++ src/modules/invoices/invoice.service.js
@@ -1,7 +1,5 @@
-// src/modules/invoices/invoice.service.js
-import Invoice from './invoice.model.js';
+import Invoice from "./invoice.model.js";
 
-// সার্ভিস: ইউজার অনুযায়ী সমস্ত ইনভয়েস খুঁজে বের করা
 export const getInvoicesByUserId = async (userId) => {
-    return await Invoice.find({ user: userId });
+  return await Invoice.find({ user: userId });
 };
