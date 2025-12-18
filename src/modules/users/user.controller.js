c690a4fceb5667cf8069e1909d0fcc8a3d4c5225
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-18 16:43:49 +0600

    user-controller

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -13,7 +13,10 @@ export const updateUser = async (req, res, next) => {
     const user = await userService.updateUserById(req.params.id, req.body);
     res.json(user);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({
+      message: error.message,
+      success: false,
+    });
   }
 };
 
@@ -22,6 +25,9 @@ export const deleteUser = async (req, res, next) => {
     const result = await userService.deleteUserById(req.params.id);
     res.json(result);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({
+      message: error.message,
+      success: false,
+    });
   }
 };
